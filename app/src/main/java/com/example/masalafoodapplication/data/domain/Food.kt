73790c3325274c3610be54cce53790213e84776f
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-17 21:03:51 +0200

    add food data class

---
+++ app/src/main/java/com/example/masalafoodapplication/data/domain/Food.kt
@@ -0,0 +1,14 @@
+package com.kiko.fillapp.data.domain
+
+
+data class Food(
+    val recipeName: String,
+    val ingredients: String,
+    val timeMinutes: String,
+    val Cuisine: String,
+    val makeRecipe: String,
+    val url: String,
+    val cleaned: String,
+    val imageUrl: String,
+    val count: String,
+)
