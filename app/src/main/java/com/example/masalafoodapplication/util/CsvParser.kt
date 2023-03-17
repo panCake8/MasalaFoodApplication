eddf9228a45a2223edfa47a6e3a54adb21baffac
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-17 21:04:09 +0200

    add csv parser

---
+++ app/src/main/java/com/example/masalafoodapplication/util/CsvParser.kt
@@ -0,0 +1,20 @@
+package com.example.masalafoodapplication.util
+
+import com.kiko.fillapp.data.domain.Food
+
+class CsvParser {
+    fun parse(line: String): Food {
+        val tokens = line.split(",")
+        return Food(
+            recipeName = tokens[Constants.ColumnIndex.RECIPE_NAME],
+            ingredients = tokens[Constants.ColumnIndex.INGREDIENTS],
+            timeMinutes = tokens[Constants.ColumnIndex.TIME_MINUTES],
+            Cuisine = tokens[Constants.ColumnIndex.CUISINE],
+            makeRecipe = tokens[Constants.ColumnIndex.HOW_TO_MAKE_RECIPE],
+            url = tokens[Constants.ColumnIndex.URL],
+            cleaned = tokens[Constants.ColumnIndex.CLEANED],
+            imageUrl = tokens[Constants.ColumnIndex.IMAGE_URL],
+            count = tokens[Constants.ColumnIndex.COUNT],
+        )
+    }
+}
