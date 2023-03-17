6df1f1f9dfe2593545a68ee9e9570e5a2c41019c
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-17 18:44:35 +0200

    create base activity

--- app/src/main/java/com/example/masalafoodapplication/BaseActivity.kt
+++ app/src/main/java/com/example/masalafoodapplication/BaseActivity.kt
@@ -3,7 +3,7 @@ package com.example.masalafoodapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
