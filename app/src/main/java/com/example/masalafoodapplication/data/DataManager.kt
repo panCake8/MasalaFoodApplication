44fa26d47fefa38c908cdd5c12caacbe3236f9df
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-17 21:43:04 +0200

    update strings

--- app/src/main/java/com/example/masalafoodapplication/data/DataManager.kt
+++ app/src/main/java/com/example/masalafoodapplication/data/DataManager.kt
@@ -4,17 +4,16 @@ import com.kiko.fillapp.data.domain.Food
 
 object DataManager {
     private val foodsList = mutableListOf<Food>()
+
     fun addFood(food: Food) {
         foodsList.add(food)
     }
 
-    fun getFood() = foodsList
+    fun getAllFood() = foodsList
 
 
     fun search(value: String) =
         foodsList.filter {
             it.timeMinutes == value
         }
-
-    fun getAllFood(): List<Food> = foodsList
 }
