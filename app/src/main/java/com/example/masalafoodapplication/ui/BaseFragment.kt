6621daeb7ff86dbde1b4336837e51902cca114cf
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-19 14:29:42 +0200

    remove imports

--- app/src/main/java/com/example/masalafoodapplication/ui/BaseFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/BaseFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.example.masalafoodapplication.util.Constants
-import com.kiko.fillapp.data.domain.Food
 
 abstract class BaseFragment<VB : ViewBinding> : Fragment() {
 
