fa345b20df23a51206b07ac1cace0814ffe3c91a
ay0o5h <aayosh553@gmail.com>
2023-03-19 22:36:33 +0300

    add fragment class

---
+++ app/src/main/java/com/example/masalafoodapplication/ui/IngredientFragment.kt
@@ -0,0 +1,22 @@
+package com.example.masalafoodapplication.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.masalafoodapplication.databinding.FragmentIngredientBinding
+
+
+class IngredientFragment : BaseFragment<FragmentIngredientBinding>() {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentIngredientBinding
+        get() = FragmentIngredientBinding::inflate
+
+    override fun setup() {
+
+    }
+
+    override fun onClicks() {
+
+    }
+
+
+}
