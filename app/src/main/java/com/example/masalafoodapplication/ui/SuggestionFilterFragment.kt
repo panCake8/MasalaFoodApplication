72fcb78f7d98cb1ad3ca426c10c94b827521fbfd
abdulaziz muthanna <a.azoz9991@gmail.com>
2023-03-19 23:06:01 +0300

    init code of filter page

---
+++ app/src/main/java/com/example/masalafoodapplication/ui/SuggestionFilterFragment.kt
@@ -0,0 +1,22 @@
+package com.example.masalafoodapplication.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.masalafoodapplication.databinding.FragmentSuggestionFilterBinding
+
+
+class SuggestionFilterFragment : BaseFragment<FragmentSuggestionFilterBinding>() {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentSuggestionFilterBinding
+        get() = FragmentSuggestionFilterBinding::inflate
+
+    override fun setup() {
+
+    }
+
+    override fun onClicks() {
+
+    }
+
+
+}
