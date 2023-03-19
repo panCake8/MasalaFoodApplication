ce353d3374759b0f133986c1df592d628265f317
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-19 19:02:16 +0200

    add details kitchen fragment

---
+++ app/src/main/java/com/example/masalafoodapplication/ui/DetailsKitchenFragment.kt
@@ -0,0 +1,20 @@
+package com.example.masalafoodapplication.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.masalafoodapplication.databinding.FragmentDetailsKitchenBinding
+
+
+class DetailsKitchenFragment : BaseFragment<FragmentDetailsKitchenBinding>() {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentDetailsKitchenBinding
+        get() = FragmentDetailsKitchenBinding::inflate
+
+    override fun setup() {
+
+    }
+
+    override fun onClicks() {
+
+    }
+
+}
