1c57c581ef4499137ced8b54075183568d54862e
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-03-21 17:42:25 +0300

    add more chard in xml and test back button

--- app/src/main/java/com/example/masalafoodapplication/ui/RecipesMenuFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/RecipesMenuFragment.kt
@@ -1,4 +1,5 @@
 package com.example.masalafoodapplication.ui
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.masalafoodapplication.databinding.FragmentRecipesMenuBinding
@@ -8,10 +9,17 @@ class RecipesMenuFragment :BaseFragment<FragmentRecipesMenuBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentRecipesMenuBinding
         get() = FragmentRecipesMenuBinding::inflate
 
+    override fun onStart() {
+        super.onStart()
+        onClicks()
+    }
+
     override fun setup() {
     }
 
     override fun onClicks() {
-
+    binding.icArrowBack.setOnClickListener{
+        Log.d("test","clicked")
+    }
     }
 }
