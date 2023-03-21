4168541c42038fc098c115ac076f801d3c8c4beb
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-22 00:26:00 +0200

    update layout and fix fragments

--- app/src/main/java/com/example/masalafoodapplication/ui/HomeFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/HomeFragment.kt
@@ -10,13 +10,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
 
+
     override fun setup() {
 
     }
 
     override fun onClicks() {
 
-    }
 
+    }
 
 }
