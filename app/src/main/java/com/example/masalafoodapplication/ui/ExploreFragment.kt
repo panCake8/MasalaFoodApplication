4d005cac5359ff4686952810ce2df767c23f0b08
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-22 03:35:17 +0200

    update logic

--- app/src/main/java/com/example/masalafoodapplication/ui/ExploreFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/ExploreFragment.kt
@@ -10,6 +10,7 @@ import com.example.masalafoodapplication.R
 import com.example.masalafoodapplication.data.DataManager
 import com.example.masalafoodapplication.databinding.FragmentExploreBinding
 import com.example.masalafoodapplication.util.loadImage
+import com.kiko.fillapp.data.domain.Food
 
 
 class ExploreFragment : BaseFragment<FragmentExploreBinding>() {
@@ -22,49 +23,56 @@ class ExploreFragment : BaseFragment<FragmentExploreBinding>() {
     }
 
     override fun onClicks() {
-
         binding.searchBar.addTextChangedListener { text ->
-            if (text?.isEmpty() == true) {
+            val list = DataManager.search(text.toString().lowercase())
+            getData(text.toString(), list)
+        }
+    }
+
+
+    private fun getData(value: String?, list: List<Food>) {
+        if (value?.isEmpty() == true) {
+            hideDataShowLottie()
+            lottieTryToSearch()
+        } else {
+            if (list.isEmpty()) {
                 hideDataShowLottie()
-                lottieTryToSearch()
-            } else {
-                val list = DataManager.search(text.toString().lowercase())
-                if (list.isEmpty()) {
-                    hideDataShowLottie()
-                    lottieNotFoundSearch()
-                    return@addTextChangedListener
-                }
-                try {
-                    showDataHideLottie()
-                    binding.apply {
-                        listOf(
-                            imvFood1, imvFood2,
-                            imvFood3, imvFood4,
-                            imvFood5, imvFood6,
-                        ).forEachIndexed { index, imv ->
-                            setImages(list[index].imageUrl, imv)
-                        }
-                        listOf(
-                            txvFoodName1, txvFoodName2,
-                            txvFoodName3, txvFoodName4,
-                            txvFoodName5, txvFoodName6,
-                        ).forEachIndexed { index, tv ->
-                            setTitles(list[index].cuisine, tv)
-                        }
-                        listOf(
-                            descriptionTv1, descriptionTv2,
-                            descriptionTv3, descriptionTv4,
-                            descriptionTv5, descriptionTv6,
-                        ).forEachIndexed { index, dv ->
-                            setTitles(list[index].makeRecipe, dv)
-                        }
-                    }
-                } catch (e: Exception) {
-                }
+                lottieNotFoundSearch()
+                return
+            }
+            try {
+                showDataHideLottie()
+                bindSearchResult(list)
+            } catch (e: Exception) {
             }
         }
     }
 
+    private fun bindSearchResult(list: List<Food>) {
+        binding.apply {
+            listOf(
+                imvFood1, imvFood2,
+                imvFood3, imvFood4,
+                imvFood5, imvFood6,
+            ).forEachIndexed { index, imv ->
+                setImages(list[index].imageUrl, imv)
+            }
+            listOf(
+                txvFoodName1, txvFoodName2,
+                txvFoodName3, txvFoodName4,
+                txvFoodName5, txvFoodName6,
+            ).forEachIndexed { index, tv ->
+                setTitles(list[index].cuisine, tv)
+            }
+            listOf(
+                descriptionTv1, descriptionTv2,
+                descriptionTv3, descriptionTv4,
+                descriptionTv5, descriptionTv6,
+            ).forEachIndexed { index, dv ->
+                setTitles(list[index].makeRecipe, dv)
+            }
+        }
+    }
 
     private fun showDataHideLottie() {
         binding.cardContainers.visibility = View.VISIBLE
