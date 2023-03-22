850ac36bb0fca149b33997a2f54cf3eb042f2da3
youssef <yr22990@gmail.com>
2023-03-22 05:10:30 +0200

    update fragment_food_detail.kt

--- app/src/main/java/com/example/masalafoodapplication/ui/FoodDetailFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/FoodDetailFragment.kt
@@ -1,13 +1,9 @@
 package com.example.masalafoodapplication.ui
 
-import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.IdRes
-import androidx.databinding.DataBindingUtil.setContentView
 import com.example.masalafoodapplication.databinding.FragmentFoodDetailBinding
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
@@ -19,39 +15,29 @@ class FoodDetailFragment : BaseFragment<FragmentFoodDetailBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentFoodDetailBinding
         get() = FragmentFoodDetailBinding::inflate
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        buttonClicked()
-    }
-
     override fun setup() {
-        binding.chip.setOnClickListener {
-            Toast.makeText(requireContext(), binding.chip.text, Toast.LENGTH_SHORT).show()
-        }
+        var food = arguments?.getParcelable<Food>("KEY")
+        chooseChips(food)
     }
 
     override fun onClicks() {
-        chooseChips()
-        buttonClicked()
     }
 
-    private fun buttonClicked() {
-        binding.d.setOnClickListener {
-            Toast.makeText(requireContext(), binding.chip.text, Toast.LENGTH_SHORT).show()
-        }
-    }
 
-    private fun chooseChips() {
+
+    private fun chooseChips(food: Food?) {
         binding.chiceGroupChips.setOnCheckedChangeListener(object : ChipGroup.OnCheckedChangeListener {
             override fun onCheckedChanged(group: ChipGroup, @IdRes checkedId: Int) {
                 val chip: Chip? = group?.findViewById(checkedId)
                 chip?.let {
                     Log.i("TAG", "chooseChips: ${it.text}")
-                    binding.descriptionTv.text = it.text
+                    binding.descriptionTv.text = food?.makeRecipe ?: "empty"
                     if (it.text == "Ingradient") {
-                        binding.descriptionTv.text = it.text
+                        binding.descriptionTv.text = food?.ingredients?.split(";")?.joinToString(separator ="\n")
+                            ?: "empty"
                     } else if (it.text == "Steps") {
-                        binding.descriptionTv.text = it.text
+                        binding.descriptionTv.text = food?.ingredients?.split(";")?.joinToString(separator ="\n")
+                            ?: "empty"
                     }
                 }
             }
