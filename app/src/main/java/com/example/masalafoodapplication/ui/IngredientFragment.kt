f69c6d6e1d9de223080c78f3131ee316c9c768ce
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-22 03:58:47 +0200

    update ingredient

--- app/src/main/java/com/example/masalafoodapplication/ui/IngredientFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/IngredientFragment.kt
@@ -1,11 +1,11 @@
 package com.example.masalafoodapplication.ui
 
 import android.content.res.ColorStateList
+import android.util.Log
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.commit
@@ -22,23 +22,24 @@ class IngredientFragment : BaseFragment<FragmentIngredientBinding>() {
         get() = FragmentIngredientBinding::inflate
 
     override fun setup() {
-        getIngredient( DataManager.getAllFood()[0])
+        getIngredient(DataManager.getAllFood()[0])
     }
 
     override fun onClicks() {
-        binding.appBar.setOnClickListener {
+        binding.topAppBar.setNavigationOnClickListener {
             requireActivity().onBackPressed()
         }
-        binding.nexBtn.setOnClickListener{
+        binding.nexBtn.setOnClickListener {
             parentFragmentManager.commit {
-                replace(R.id.fragment_container,IngredientFragment())
+                replace(R.id.fragment_container, IngredientFragment())
                     .addToBackStack("ingredient")
             }
         }
     }
-    private fun getIngredient(foods:Food){
-       val options=  foods.ingredients.split(";").toTypedArray()
-        val linearLayoutOptions=binding.checkboxLayout
+
+    private fun getIngredient(foods: Food) {
+        val options = foods.ingredients.split(";").toTypedArray()
+        val linearLayoutOptions = binding.checkboxLayout
         for (option in options) {
             val checkBox = MaterialCheckBox(context)
             checkBox.id = View.generateViewId()
@@ -46,16 +47,24 @@ class IngredientFragment : BaseFragment<FragmentIngredientBinding>() {
             checkBox.height = ViewGroup.LayoutParams.WRAP_CONTENT
             checkBox.text = option
             checkBox.layoutDirection = View.LAYOUT_DIRECTION_RTL
-            checkBox.buttonTintList = ColorStateList.valueOf(ContextCompat.getColor(binding.root.context, R.color.base_color))
+            checkBox.buttonTintList = ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    binding.root.context,
+                    R.color.base_color
+                )
+            )
             checkBox.isChecked = false
-            checkBox.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_medium))
+            checkBox.setTextSize(
+                TypedValue.COMPLEX_UNIT_PX,
+                resources.getDimension(R.dimen.text_medium)
+            )
             checkBox.typeface = ResourcesCompat.getFont(binding.root.context, R.font.work_sans)
             linearLayoutOptions.addView(checkBox)
             checkBox.setOnCheckedChangeListener { _, isChecked ->
                 if (isChecked) {
-                    print("Checked")
+                    Log.d("TAG", "checked")
                 } else {
-                    print("not Checked")
+                    Log.d("TAG", "non checked")
                 }
             }
         }
