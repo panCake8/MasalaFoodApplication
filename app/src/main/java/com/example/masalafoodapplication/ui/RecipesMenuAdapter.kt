ea93dad4c0b47a796cecaa1b6e44ec4cdddfd4ab
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-03-23 17:02:41 +0300

    make one fragment and one adapter to show recipes menu

--- app/src/main/java/com/example/masalafoodapplication/ui/RecipesMenuAdapter.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/RecipesMenuAdapter.kt
@@ -6,10 +6,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.masalafoodapplication.R
 import com.example.masalafoodapplication.databinding.ItemRecipesBinding
+import com.example.masalafoodapplication.util.loadImage
 
 import com.kiko.fillapp.data.domain.Food
 
-class RecipesAdapter (val list: List<Food>): RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder>(){
+class RecipesMenuAdapter (val list: List<Food>): RecyclerView.Adapter<RecipesMenuAdapter.RecipesViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesViewHolder {
         val view =LayoutInflater.from(parent.context).inflate(R.layout.item_recipes,parent,false)
         return  RecipesViewHolder(view)
@@ -20,11 +21,11 @@ class RecipesAdapter (val list: List<Food>): RecyclerView.Adapter<RecipesAdapter
     override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
         val currentRecipe =list[position]
         holder.binding.apply {
-           recipeName1.text = currentRecipe.recipeName
-            prepareTime1.text = currentRecipe.timeMinutes.toString()
+
+            recipeName1.text = currentRecipe.recipeName
+            prepareTime1.text = currentRecipe.timeMinutes.toString() +"m"
+            imageRecipe1.loadImage(currentRecipe.imageUrl)
         }
-
-
     }
     class RecipesViewHolder(viewItem: View) :RecyclerView.ViewHolder(viewItem) {
         val binding = ItemRecipesBinding.bind(viewItem)
--- app/src/main/java/com/example/masalafoodapplication/ui/RecipesMenuFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/RecipesMenuFragment.kt
@@ -1,29 +1,20 @@
 package com.example.masalafoodapplication.ui
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.masalafoodapplication.data.DataManager
 import com.example.masalafoodapplication.databinding.FragmentRecipesMenuBinding
-import com.example.masalafoodapplication.util.loadImage
-import com.example.masalafoodapplication.util.setPreparationTime
-import com.kiko.fillapp.data.domain.Food
 
 class RecipesMenuFragment : BaseFragment<FragmentRecipesMenuBinding>() {
-    private lateinit var list: List<Food>
+
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentRecipesMenuBinding
         get() = FragmentRecipesMenuBinding::inflate
 
 
     override fun setup() {
-        val adapter = RecipesAdapter(DataManager.getAllFood())
+        val adapter = RecipesMenuAdapter(DataManager.getAllFood())
         binding.recyclerRecipes.adapter =adapter
-        list = DataManager.showMostQuickRecipes()
-        addImage()
-        addPrepareTime()
-        addRecipesName()
+
     }
 
     override fun onClicks() {
@@ -32,47 +23,6 @@ class RecipesMenuFragment : BaseFragment<FragmentRecipesMenuBinding>() {
         }
     }
 
-    private fun setImages(url: String, imageView: ImageView) {
-        imageView.loadImage(url)
-    }
 
-    private fun addImage() {
-//        binding.apply {
-//            listOf(
-//                image1, image2, image3, image4
-//            ).forEachIndexed { index, imageView ->
-//                setImages(list[index].imageUrl, imageView)
-//            }
-//        }
-    }
-
-    private fun addRecipesName() {
-//        binding.apply {
-//            listOf(
-//                recipeName1, recipeName2, recipeName3, recipeName4
-//            ).forEachIndexed { index, textView ->
-//                setRecipesNames(list[index].recipeName, textView)
-//            }
-//        }
-    }
-
-    private fun setRecipesNames(name: String, textView: TextView) {
-        textView.text = name
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun addPrepareTime() {
-//        binding.apply {
-//            listOf(
-//                prepareTime1, prepareTime2, prepareTime3, prepareTime4
-//            ).forEachIndexed { index, textView ->
-//                setPrepareTimes(list[index].timeMinutes, textView)
-//            }
-//        }
-    }
-
-    private fun setPrepareTimes(time: Int, textView: TextView) {
-        textView.setPreparationTime(time)
-    }
 
 }
