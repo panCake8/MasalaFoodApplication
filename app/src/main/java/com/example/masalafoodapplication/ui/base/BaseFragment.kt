5432ad1c9ecef1dfb6424195dba822e63e20d818
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-03-27 01:46:17 +0200

    add packages

--- app/src/main/java/com/example/masalafoodapplication/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/base/BaseFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import androidx.viewbinding.ViewBinding
 import com.example.masalafoodapplication.R
