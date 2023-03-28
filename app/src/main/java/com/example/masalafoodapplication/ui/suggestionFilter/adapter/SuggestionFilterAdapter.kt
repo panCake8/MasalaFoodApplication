a9ad2ec39d8896c8302cda1a5598d54b9eb34e99
abdulaziz muthanna <a.azoz9991@gmail.com>
2023-03-28 18:26:24 +0300

    fix chip alignment horizontal

--- app/src/main/java/com/example/masalafoodapplication/ui/suggestionFilter/adapter/SuggestionFilterAdapter.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/suggestionFilter/adapter/SuggestionFilterAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.masalafoodapplication.ui.ingredient.adapter
+package com.example.masalafoodapplication.ui.suggestionFilter.adapter
 
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.masalafoodapplication.R
 import com.example.masalafoodapplication.databinding.ItemIngredientChipsBinding
 
-class IngredientChipAdapter(val list: MutableList<String>,val listener: IngredientChipInteractionListener)  : RecyclerView.Adapter<IngredientChipAdapter.IngredientViewHolder>() {
+class SuggestionFilterAdapter(val list: MutableList<String>, val listener: SuggestionFilterInteractionListener)  : RecyclerView.Adapter<SuggestionFilterAdapter.IngredientViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_ingredient_chips,parent,false)
         return IngredientViewHolder(view)
