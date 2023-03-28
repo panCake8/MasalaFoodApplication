f9612a5425295c1a1c8899b61be4d61478979c08
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-03-29 02:26:24 +0300

    move home items

--- app/src/main/java/com/example/masalafoodapplication/data/domain/models/HomeItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.masalafoodapplication.data.domain.models
-
-import com.example.masalafoodapplication.data.domain.enums.HomeItemType
-
-data class HomeItem<T>(
-    val data: T,
-    val type: HomeItemType,
-)
