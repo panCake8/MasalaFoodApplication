4e314dee33740f449fe945d99cf85296d5b8a521
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2023-03-28 20:30:56 +0200

    fix history

--- app/src/main/java/com/example/masalafoodapplication/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/masalafoodapplication/ui/history/HistoryFragment.kt
@@ -16,7 +16,7 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding>() {
     }
 
     override fun onClicks() {
-        binding.historyToolbar.setOnClickListener {
+        binding.toolBarHistory.setOnClickListener {
             onBack()
         }
 
